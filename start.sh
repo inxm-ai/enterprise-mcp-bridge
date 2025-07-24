34ff4cea64c2bcbc4f9327468b296413593db2c1
Matthias Kainer <mail@matthias-kainer.de>
2025-07-24 09:04:45 +0200

    Switched from alpine to bullseye

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$#" -gt 0 ]; then
   export MCP_SERVER_COMMAND="$*"
@@ -7,6 +7,10 @@ fi
 # for development purposes another pip install can be triggered in the /mcp directory
 if [ "$ENV" == "dev" ]; then
   if [ -d "/mcp" ]; then
+    if [ -f "/mcp/install.sh" ]; then
+      echo "Running /mcp/install.sh"
+      sh /mcp/install.sh
+    fi
     if [ -f "/mcp/requirements.txt" ]; then
       echo "Installing additional requirements from /mcp/requirements.txt"
       pip install -r /mcp/requirements.txt
