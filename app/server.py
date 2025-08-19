d6642773762ae5a48efd891361cb5adfa9fc5c61
Matthias Kainer <mail@matthias-kainer.de>
2025-08-19 08:56:45 +0200

    Add prometheus metrics

--- app/server.py
+++ app/server.py
@@ -1,6 +1,8 @@
-
 from fastapi import FastAPI
 from .routes import router
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+instrumentator = Instrumentator()
 app.include_router(router)
+instrumentator.instrument(app).expose(app)
